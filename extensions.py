b8c1a2e8e5326d761c2ea1e2d3813fa76d14609d
bbldaaa750 <bbldaaa750@gmail.com>
2025-12-04 06:39:50 +0300

    refactor models and extensions

--- extensions.py
+++ extensions.py
@@ -1,3 +1,7 @@
 ﻿from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db: SQLAlchemy = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
