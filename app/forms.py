a6488f57ada3282f7cdfa00dcfc4a831b6f86752
bbldaaa750 <bbldaaa750@gmail.com>
2025-12-07 05:20:28 +0300

    feature: deck form with flask-wtf

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class DeckForm(FlaskForm):
+    title = StringField('Название колоды', validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField('Создать')
--- app/views/deck.py
+++ app/views/deck.py
@@ -1,27 +1,26 @@
 ﻿from flask import Blueprint, request, render_template, redirect, url_for, flash
 from app.services import create_deck, get_deck, get_user_decks, update_deck, delete_deck
 from flask_login import login_required, current_user
+from app.forms import DeckForm
 
 bp = Blueprint('deck', __name__, url_prefix='/deck')
 
 @bp.route('/', methods=['GET', 'POST']) 
 @login_required 
 def index():
-    if request.method == 'POST':
-        title = request.form.get('title') 
-        if title:
-            try:
-                create_deck(current_user, title)
-                flash(f'Колода "{title}" создана!', 'success')
-            except ValueError:
-                flash('Колода уже существует!', 'error')
-        else:
-            flash('Название не может быть пустым', 'error')
-        
-        return redirect(url_for('deck.index'))
-
+    form = DeckForm()
+    if form.validate_on_submit():
+        title = form.title.data 
+        try:
+            create_deck(current_user, title)
+            flash(f'Колода "{title}" создана!', 'success')
+            return redirect(url_for('deck.index'))
+        except ValueError:
+            flash('Колода уже существует!', 'error')
+            
     user_decks = get_user_decks(current_user)
-    return render_template('deck.html', decks=user_decks)
+    return render_template('deck.html', decks=user_decks, form=form)
+
 
 @bp.route('/<int:deck_id>/delete', methods=['POST'])
 @login_required
